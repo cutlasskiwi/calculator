039b6cabf6046c2ead0fa1787d3f4c843260342d
cutlasskiwi <ahhsandra@gmail.com>
2020-10-08 23:17:11 +0200

    calculater final push

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const numberKeys = document.querySelectorAll();
+//Defined variables:
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector(".screen");
+
+//Button function:
+buttons.forEach(function (button) {
+  button.addEventListener("click", calculate);
+});
+
+// Calculate action:
+function calculate(event) {
+  // current clicked buttons value
+  const clickedButtonValue = event.target.value;
+
+  if (clickedButtonValue === "=") {
+    if (display.value !== "") {
+      display.value = eval(display.value);
+    }
+  } else if (clickedButtonValue === "clear") {
+    display.value = "";
+  } else {
+    display.value += clickedButtonValue;
+  }
+}
